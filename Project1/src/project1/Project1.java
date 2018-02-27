221713fe99a78a0331722f53bc57ca8de5e2ef77
Emily <embox4@gmail.com>
2018-02-27 14:19:41 -0600

    CBC update

--- Project1/src/project1/Project1.java
+++ Project1/src/project1/Project1.java
@@ -11,51 +11,56 @@ import java.math.BigInteger;
  *
  * @author darek
  */
-public class project1 {
+public class Project1 {
     public static void main(String[] args){
-         /*
-        //Shift Cipher Test
-        ShiftCipher SCcode = new ShiftCipher();
-        BigInteger SCplainMessage, SCcipherMessage, SCdecodedMessage, SCkey;
-        SCplainMessage = new BigInteger("15");
-        SCkey = new BigInteger("5");
         
-        //Print out original message and key
-        System.out.println("Message: " + SCplainMessage);
-        System.out.println("Key: " + SCkey);
-        
-        //Print out encoded message
-        SCcipherMessage = SCcode.encrypt(SCplainMessage, SCkey);
-        System.out.println("Encrypted Message: " + SCcipherMessage);
-        
-        //Print out decoded message
-        SCdecodedMessage = SCcode.decrypt(SCcipherMessage, SCkey);
-        System.out.println("Decrypted Message: " + SCdecodedMessage);
-        
-        //CBC Test
-        CBC CBCcode = new CBC();
-        BigInteger CBCplainMessage, CBCcipherMessage, CBCdecodedMessage, CBCkey;
-        CBCplainMessage = new BigInteger("010110001110");
-        CBCkey = new BigInteger("010");
-        
-        //Print out original message and key
-        System.out.println("Message: " + CBCplainMessage);
-        System.out.println("Key: " + CBCkey);
-        
-        //Print out encoded message
-        CBCcipherMessage = CBCcode.encrypt(CBCplainMessage, CBCkey);
-        System.out.println("Encrypted Message: " + CBCcipherMessage);
-        
-        //Print out decoded message
-        CBCdecodedMessage = CBCcode.decrypt(CBCcipherMessage, CBCkey);
-        System.out.println("Decrypted Message: " + CBCdecodedMessage);
-        
-        
-       
-    */
-        testMAC();
+        //testShiftCipher() functional
+        testCBC();
+        //testPoly
+        //testMAC();
     }
     
+        public static void testShiftCipher()
+        {
+            //Shift Cipher Test
+            ShiftCipher SCcode = new ShiftCipher();
+            BigInteger SCplainMessage, SCcipherMessage, SCdecodedMessage, SCkey;
+            SCplainMessage = new BigInteger("15");
+            SCkey = new BigInteger("5");
+        
+            //Print out original message and key
+            System.out.println("Message: " + SCplainMessage);
+            System.out.println("Key: " + SCkey);
+        
+            //Print out encoded message
+            SCcipherMessage = SCcode.encrypt(SCplainMessage, SCkey);
+            System.out.println("Encrypted Message: " + SCcipherMessage);
+            
+            //Print out decoded message
+            SCdecodedMessage = SCcode.decrypt(SCcipherMessage, SCkey);
+            System.out.println("Decrypted Message: " + SCdecodedMessage);
+        }
+        
+        public static void testCBC()
+        {
+            CBC CBCcode = new CBC();
+            BigInteger CBCplainMessage, CBCcipherMessage, CBCdecodedMessage, CBCkey;
+            CBCplainMessage = new BigInteger("23");
+            CBCkey = new BigInteger("5");
+            
+            //Print out original message and key
+            System.out.println("Message: " + CBCplainMessage);
+            System.out.println("Key: " + CBCkey);
+            
+            //Print out encoded message
+            CBCcipherMessage = CBCcode.encrypt(CBCplainMessage, CBCkey);
+            System.out.println("Encrypted Message: " + CBCcipherMessage);
+            
+            //Print out decoded message
+            CBCdecodedMessage = CBCcode.decrypt(CBCcipherMessage, CBCkey);
+            System.out.println("Decrypted Message: " + CBCdecodedMessage);
+        }
+        
         public static void testPoly(){
             PolyalphabeticCipher test = new PolyalphabeticCipher();
             BigInteger key = new BigInteger("1234");
@@ -66,6 +71,7 @@ public class project1 {
             System.out.println("Decrypted Message: "  + test.decrypt(result, key));
     
         }
+        
         public static void testMAC(){
             MacCipher test = new MacCipher();
             BigInteger plain = new BigInteger("15015");
