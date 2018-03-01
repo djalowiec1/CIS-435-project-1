97a1fca0125de6161142711d1c9d11bb9e1a4107
camconk <cconklin@mail.bradley.edu>
2018-03-01 14:29:49 -0600

    Addd CA

---
+++ Project1/src/project1/CA.java
@@ -0,0 +1,55 @@
+/*
+ *This class verifies a users ID with their Key
+ *
+ *@author <Cameron Conklin>
+ *@date <3/1/2018>
+ */
+
+
+package cipher;
+import java.math.*;
+public class CA {
+
+	BigInteger[] people = new BigInteger[4];
+	
+	
+	private void fill() {
+		BigInteger x,z,c,v;
+		x = new BigInteger("12");
+		z= new BigInteger("45");
+		c= new BigInteger("22");
+		v= new BigInteger("78");
+		
+		people[0] = x;
+		people[1] = z;
+		people[2] = c;
+		people[3] = v;
+		
+		}
+	
+	
+	public boolean certify(BigInteger ID, BigInteger Key) {
+		 
+		fill();
+		boolean result = false;
+		
+		
+		
+
+		int p = ID.intValue();
+		System.out.println(p);
+		for (int x=0;x<people.length;x++) {
+			if (people[p].equals(Key)) {
+				result = true;
+			}
+			else result = false;
+		
+	
+	}
+		return result;
+	
+	
+		
+		
+	}		
+}
