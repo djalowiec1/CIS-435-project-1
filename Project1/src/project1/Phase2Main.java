46b450a192f8ffaf7889918c5329376838a5e8ae
tcallahan09 <36520256+tcallahan09@users.noreply.github.com>
2018-03-22 12:07:43 -0500

    created phase 2 main

---
+++ Project1/src/project1/Phase2Main.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project1;
+
+/**
+ *
+ * @author tcallahan
+ */
+public class Phase2Main {
+    public static void main(String[] args){
+        System.out.println("***** Secure Message System Simulation ***** ");
+        
+        Sender sndr = new Sender();
+        System.out.println("Sender is Created");
+//        Receiver rcvr = new Receiver();
+//        Network ntwk = new Network();
+        
+           
+    }
+}
