506a6ed84ea300ad37230680e5b03a95828d49fc
camconk <cconklin@mail.bradley.edu>
2018-03-29 13:45:20 -0500

    Update CA.java

--- Project1/src/project1/CA.java
+++ Project1/src/project1/CA.java
@@ -9,43 +9,8 @@
 package project1;
 import java.math.*;
 public class CA {
-<<<<<<< HEAD
-
-    public BigInteger[][] people;
-    BigInteger empty;
-    
-    public CA(){
-        people = new BigInteger[2][1000];
-        empty = BigInteger.ZERO;
-        
-        
-    }
-
-    public void fill() {
-            BigInteger a,b,c,d,e;
-            a = new BigInteger("12");
-            b= new BigInteger("45");
-            c= new BigInteger("22");
-            d= new BigInteger("78");
-            e= new BigInteger("0");
-
-            people[0][0] = a;
-            people[1][0] = a;
-            people[0][1] = a;
-            people[1][1] = a;		
-            people[0][2] = a;
-            people[1][2] = a;
-            people[0][3] = a;
-            people[1][3] = a;
-
-
-
-          for(int i=0; i<people.length; i++) {
-            for(int j=4; j<people[i].length; j++) {
-             people[i][j] = e;
-=======
-	BigInteger x = new BigInteger ("2^200"); 
-	public BigInteger[][] people = new BigInteger[2][x];
+	
+	public BigInteger[][] people = new BigInteger[2][99999];
 	BigInteger empty = new BigInteger("0");
 	
 	public void fill() {
@@ -81,48 +46,40 @@ public class CA {
             if (people[0][p].equals(empty)&&people[1][p].equals(empty)){
                 people[0][p].equals(Key[0]);
                 people[1][p].equals(Key[1]);
->>>>>>> 24e6ba60ebc962179b7af26ff729f1c479d5e576
             }
-          }
-    }
-
-    public BigInteger register(BigInteger ID, BigInteger Key[]){
-
-        int p = ID.intValue();
-
-        if (people[0][p].equals(empty)&&people[1][p].equals(empty)){
-            people[0][p].equals(Key[0]);
-            people[1][p].equals(Key[1]);
-        }
-        else{
-            while (!(people[0][p].equals(empty)&&people[1][p].equals(empty))){
-                //System.out.println(people[p]);
-                p++;
+            else{
+                while (!(people[0][p].equals(empty)&&people[1][p].equals(empty))){
+                    //System.out.println(people[p]);
+                    p++;
+                }
+                System.out.println("Given ID is unavailable. Genereating new ID...");
+                System.out.println("New ID is " + p);
+                people[0][p].equals(Key[0]);
+                people[1][p].equals(Key[1]);
+             
+                
             }
-            System.out.println("Given ID is unavailable. Genereating new ID...");
-            System.out.println("New ID is " + p);
-            people[0][p].equals(Key[0]);
-            people[1][p].equals(Key[1]);
-
-
+                           
+             BigInteger ID_registered = BigInteger.valueOf(p);
+                                 
+        return ID_registered;    
         }
-
-         BigInteger ID_registered = BigInteger.valueOf(p);
-
-    return ID_registered;    
-    }
-
-
-    public BigInteger[] getKey(BigInteger ID) {
-
-            BigInteger Key[] = new BigInteger[2];
-
-            int p = ID.intValue();
-
-            Key[0] = people[0][p];
-            Key[1] = people[1][p];
-
-            return Key;
-    }		
+        
+       
+	public BigInteger[] getKey(BigInteger ID) {
+		 
+		BigInteger Key[] = new BigInteger[2];
+                
+		int p = ID.intValue();
+                
+		Key[0] = people[0][p];
+                Key[1] = people[1][p];
+		
+		return Key;
+	
+	
+		
+		
+	}		
 }
 
